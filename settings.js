a843fdaa51e5fb31d0fcd2c21e0343f577ea3901
mrhussainxten <xtenhussain@gmail.com>
2025-02-09 11:41:19 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Hᴜssᴀɪɴ 〆ᬊᬁ',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "𝗠𝗥_𝗛𝗨𝗦𝗦𝗔𝗜𝗡",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '+8801334089349', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
